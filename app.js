f08a6e51f8b3f4b22fb79d69053ed27885dce9b6
joaoadavid <joao.antoniodavid@hotmail.com>
2023-10-27 20:32:21 -0300

    rotas post e delete

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 const express = require('express');
-
+const checklistRouter = require('./src/routes/checklist')
 const app = express();
 
 app.use(express.json());
 
-const log = (req, res, next) => {
-  console.log(req.body);
-  console.log(`Data: ${Date.now()}`);
-  next();
-}
+// nesse formato, define uma pré rota derivando da router
+app.use('/checklists', checklistRouter);//criado
 
-app.use(log);
-
-app.get('/', (req, res) => {
-  res.send('<h1>Minha Lista de tarefas :)</h1>')
-})
-
-app.get('/json', (req, res) => {
-  console.log(req.body);
-  res.json({ title: 'tarefa X', done: true });
-})
 
 app.listen(3000, () => {
   console.log('Servidor foi iniciado')
